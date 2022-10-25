85081fa09c1fa96e0a45b995c6e666df39bb36d1
Will Pierson <willcpierson@gmail.com>
2022-10-25 15:44:06 -0400

    add playlist CSS/test songItems

--- frontend/src/components/Playlist/Playlist.js
+++ frontend/src/components/Playlist/Playlist.js
@@ -3,12 +3,23 @@ import styles from './Playlist.module.css'
 
 const Playlist = () => {
 
+    const testArray = ['Song 1', 'Song 2', 'Song 3', 'Song 4', 'Song 5', 'Song Test', 'Song Test', 'Song Test', 'Song Test', 'Song Test', 'Song Test', 'Song Test', 'Song Test', 'Song Test', 'Song Test', 'Song Test', ]
+
+
+    const mappedSongs = testArray.map((song) => {
+        return <li className={styles.songListItems}> {song} </li>
+    })
 
     return (
         <>
-            <aside id={styles.playlistContainer}>
-
-            </aside>
+            <div id={styles.allOfPlaylist}>
+                <h2 id={styles.header}>Playlist</h2>
+                <aside id={styles.playlistContainer}>
+                    <ul id={styles.songList}>
+                        {mappedSongs}
+                    </ul>
+                </aside>
+            </div>
         </>
     );
 };
