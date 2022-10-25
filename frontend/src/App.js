dd436bdfbe72c2613c207ae82cc0a135f4cfb976
May Wu <maywu4@gmail.com>
2022-10-25 10:48:08 -0400

    Update login and sign up modals

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,8 +5,6 @@ import { AuthRoute, ProtectedRoute } from './components/Routes/Routes';
 import NavBar from './components/NavBar/NavBar';
 
 import MainPage from './components/MainPage/MainPage';
-// import LoginForm from './components/SessionForms/LoginForm';
-// import SignupForm from './components/SessionForms/SignupForm';
 import { getCurrentUser } from './store/session';
 
 
