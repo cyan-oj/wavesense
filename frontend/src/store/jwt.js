e1c55c5d35f35b96475ce42cd30c7cf123f8fde1
May Wu <maywu4@gmail.com>
2022-10-25 16:29:59 -0400

    Update jwtFetch to include 'bearer' in header

--- frontend/src/store/jwt.js
+++ frontend/src/store/jwt.js
@@ -15,7 +15,7 @@ async function jwtFetch(url, options = {}) {
   // Set options.headers to an empty object if there is no headers.
   options.headers = options.headers || {};
   // Set the "Authorization" header to the value of "jwtToken" in localStorage.
-  options.headers["Authorization"] = localStorage.getItem("jwtToken");
+  options.headers["Authorization"] = "bearer " + localStorage.getItem("jwtToken");;
   
   // If the options.method is not 'GET', then set the "Content-Type" header to
   // "application/json".
