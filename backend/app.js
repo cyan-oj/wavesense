8d366041903748824a0ff09616ad716053b8f7fe
Brian <brianrlam@gmail.com>
2022-10-26 15:36:39 -0400

    stuck on scrf token with aws

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,7 @@ require('./models/Song')
 require('./config/passport');
 const csurf = require('csurf');
 const debug = require('debug');
+// const expressfileupload = require('express-fileupload');
 
 const app = express();
 const passport = require('passport');
@@ -28,6 +29,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// app.use(expressfileupload)
 
 if (!isProduction) {
     app.use(cors());
--- frontend/src/components/Playlist/PlaylistSongIndex/PlaylistSongIndex.js
+++ frontend/src/components/Playlist/PlaylistSongIndex/PlaylistSongIndex.js
@@ -1,12 +1,45 @@
+import { useState } from 'react';
+import jwtFetch from '../../../store/jwt';
 import styles from './PlaylistSongIndex.module.css'
 
 
 const PlaylistSongIndex = () => {
 
+    const [title, setTitle] = useState('');
+    const [artist, setArtist] = useState('');
+    const [file, setFile] = useState(null);
+
+    const handleChange = (e)=>{
+        e.preventDefault();
+        setFile(e.currentTarget.files[0]);
+    }
+
+    const handleSubmit = async (e)=>{
+        e.preventDefault();
+        const formData = new FormData();
+        formData.append('title', title);
+        formData.append('artist', artist);
+        formData.append('audio-upload', file);
+
+        const res = await jwtFetch('/api/songs', {
+            method: 'POST',
+            body: formData
+        });
+
+        // const res2 = await res.json();
+        // console.log(res2);
+    }
 
     return (
         <>
-        
+            <h1>TEST</h1>
+            <form onSubmit={handleSubmit}>
+                <input placeholder='title' value={title} onChange={(e)=>setTitle(e.target.value)}/>
+                <input placeholder='artist' value={artist} onChange={(e)=>setArtist(e.target.value)}/>
+                <input type='file' onChange={handleChange}/>
+                <br></br>
+                <input type='submit' />
+            </form>
         </>
     );
 };
