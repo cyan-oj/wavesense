406352d0bfacb1607675543accc1a49e2a1aae43
cyan <97122638+cyan-oj@users.noreply.github.com>
2022-12-03 13:14:32 -0500

    switch analyzer to cube

--- frontend/src/components/Visualizer/Analyzer.js
+++ frontend/src/components/Visualizer/Analyzer.js
@@ -1,14 +1,20 @@
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState, useLayoutEffect } from 'react'
 import { useFrame } from '@react-three/fiber'
-import { AudioAnalyser } from 'three'
+import { AudioAnalyser, Matrix4 } from 'three'
 import { Sphere } from '@react-three/drei'
 
 const Analyzer = ({ audio }) => {
-  const mesh = useRef();
+  const grid = useRef();
+  const [hovered, hover] = useState(false);
+
   const analyser = useRef();
 
+  useLayoutEffect(() => {
+    grid.current.setMatrixAt(0, new Matrix4())
+  }, [])
+
   useEffect(() => {
-    console.log("analyzer useEffect")
+    console.log("analyzer update:", audio)
     if (audio.current) {
       analyser.current = new AudioAnalyser(audio.current, 32);
     }
@@ -17,16 +23,23 @@ const Analyzer = ({ audio }) => {
   useFrame(() => {
     if (analyser.current) {
       const data = analyser.current.getAverageFrequency();
-      mesh.current.material.color.setRGB(data / 100, 0, 0);
-      mesh.current.scale.x = mesh.current.scale.y = mesh.current.scale.z =
-        (data / 100) * 2;
+      // mesh.current.material.color.setRGB(data / 100, 0, 0);
+
+      // grid.current.rotation.x += 0.001
+      // grid.current.rotation.y += 0.001
+      grid.current.scale.y = data/100 * 2 + 0.1
     }
   });
 
   return (
-    <Sphere ref={mesh} args={[1, 64, 64]}>
-      <meshBasicMaterial />
-    </Sphere>
+    <instancedMesh
+      castShadow
+      ref={grid}
+      args={[ null, null, 1]}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={ 'orange' }/>
+    </instancedMesh>
   );
 }
 
