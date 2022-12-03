2a6b743a8f1b0486204e2b782f8c8da0df8b9920
cyan <97122638+cyan-oj@users.noreply.github.com>
2022-12-03 17:57:08 -0500

    mirrored, balanced, redundant code removed

--- frontend/src/components/Visualizer/Analyzer.js
+++ frontend/src/components/Visualizer/Analyzer.js
@@ -6,8 +6,8 @@ import { Sphere } from '@react-three/drei'
 const tempBar = new Object3D();
 
 const Analyzer = ({ audio }) => {
-  const numBars = 4
-  const barWidth = 1.2
+  const numBars = 64
+  const barWidth = 0.2
 
   const barGeo = new BoxGeometry(barWidth, 1, barWidth);
   const material = new MeshStandardMaterial({ color: "orange" });
@@ -15,31 +15,42 @@ const Analyzer = ({ audio }) => {
   const grid = useRef();
   const [hovered, hover] = useState(false);
 
-  // const data = new Uint8Array(16);
   const analyser = useRef();
 
   useEffect(() => {
     console.log( "analyzer update:", audio.current )
     if ( audio.current ) {
-      analyser.current = new AudioAnalyser( audio.current, 32 );
+      analyser.current = new AudioAnalyser( audio.current, 128 );
     }
   }, [ audio.current ]);
 
   useFrame(() => {
     if ( analyser.current ) {
       const data = analyser.current.getFrequencyData();
-      console.log(data)
+      // console.log(average)
+      // console.log(data)
       // mesh.current.material.color.setRGB(data / 100, 0, 0);
 
-      for ( let x = 0; x < numBars; x++) {
-        tempBar.position.set((barWidth * (numBars/2 - x - 1/2 )), 0, 1 )
-        tempBar.rotation.y += .01
+      tempBar.position.set(0, 0, 1 )
+      tempBar.scale.y = data[0]/50 + .001
+
+      tempBar.updateMatrix();
+      grid.current.setMatrixAt( 0, tempBar.matrix )
+      
+      for ( let x = 1; x < numBars; x++) {
+        tempBar.position.set((barWidth * (x)), 0, 1 )
         tempBar.updateMatrix();
         grid.current.setMatrixAt( x, tempBar.matrix )
+        
+        tempBar.position.set((-barWidth * (x)), 0, 1 )
+        tempBar.updateMatrix();
+        grid.current.setMatrixAt( x+numBars, tempBar.matrix )
+        
+        tempBar.scale.y = data[x]/50 + .001
+        tempBar.rotation.y += .0001
       }
 
       grid.current.instanceMatrix.needsUpdate = true;
-      grid.current.scale.y = data[2]/100 * 2 + 0.1
     }
   });
 
