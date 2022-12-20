4587499021d569edce4842efd827a44b45a9f81c
cyan <97122638+cyan-oj@users.noreply.github.com>
2022-12-20 15:12:13 -0500

    change backend port

--- frontend/src/components/Visualizer/TimeDisplay.js
+++ frontend/src/components/Visualizer/TimeDisplay.js
@@ -4,6 +4,7 @@ import styles from "./Visualizer.module.css";
 const TimeDisplay = ({ song }) => {
 
   const [time, setTime] = useState("0:00");
+  let totalTime = "0:00"
 
   const timeFormat = (seconds) => {
     const mins = Math.floor(seconds/60);
@@ -16,15 +17,16 @@ const TimeDisplay = ({ song }) => {
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setTime(timeFormat(song.currentTime))
+      setTime()
     }, 300);
 
     return () => {
       clearInterval(interval);
     }
   }, [song])
+
+  totalTime = timeFormat(song.currentTime)
   
-  const totalTime = timeFormat(song.duration)
 
   return(
     <div id={styles.timer}>
