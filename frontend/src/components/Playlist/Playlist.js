a809577aa3b123bf71c400d85fa6d7f3e87135d2
May Wu <maywu4@gmail.com>
2022-12-21 21:47:57 -0500

    update all icons

--- frontend/src/components/Playlist/Playlist.js
+++ frontend/src/components/Playlist/Playlist.js
@@ -6,12 +6,13 @@ import { getSongs, fetchSongs, deleteSong } from '../../store/songs';
 import { useEffect } from 'react';
 import Visualizer from '../Visualizer/Visualizer';
 import { Modal } from '../../context/Modal';
-
 import { fetchPlaylists } from '../../store/playlists';
 import Playlists from '../Playlists/Playlists';
 import { PlaylistCreateForm } from '../PlaylistForm/PlaylistFormModal';
 import SongUploadForm from './SongUploadForm/SongUploadForm';
 import AddSongToPlaylistMenu from './AddSongToPlaylistMenu/AddSongToPlaylistMenu';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrashCan } from '@fortawesome/free-regular-svg-icons'
 
 const Playlist = ({ songUrl, setSongUrl }) => {
     const dispatch = useDispatch();
@@ -143,7 +144,7 @@ const Playlist = ({ songUrl, setSongUrl }) => {
                             {/* <svg id={styles.trashcan}>
                             <path d="M16 6V4.5C16 3.12 14.88 2 13.5 2h-3C9.11 2 8 3.12 8 4.5V6H3v2h1.06l.81 11.21C4.98 20.78 6.28 22 7.86 22h8.27c1.58 0 2.88-1.22 3-2.79L19.93 8H21V6h-5zm-6-1.5c0-.28.22-.5.5-.5h3c.27 0 .5.22.5.5V6h-4V4.5zm7.13 14.57c-.04.52-.47.93-1 .93H7.86c-.53 0-.96-.41-1-.93L6.07 8h11.85l-.79 11.07zM9 17v-6h2v6H9zm4 0v-6h2v6h-2z"></path>
                             </svg> */}
-                            🗑️
+                        <FontAwesomeIcon icon={faTrashCan} size="lg" />
                         </p>
                         <AddSongToPlaylistMenu song={song}/>
                     </div>
@@ -169,7 +170,7 @@ const Playlist = ({ songUrl, setSongUrl }) => {
                                             <br></br>
                                             <span className={styles.artistName}>{song.artist}</span>
                                     </button>
-                                    <p className={styles.addDelete}>DELETE</p>
+                                    {/* <p className={styles.addDelete}>DELETE</p> */}
                                 </div>
                             </li>
                         )
